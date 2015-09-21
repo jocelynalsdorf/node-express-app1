97f21cb4cf027c6a3270df7df562dbeeb816b788
jocelynalsdorf <jfrodsla@gmail.com>
2015-09-21 16:00:46 -0700

    adds loop to iterate over array of post objects

--- src/app.js
+++ src/app.js
@@ -3,6 +3,11 @@ var express = require('express');
 var app = express();
 var posts = require('./mock/posts.json');
 
+//turns an object into an array
+var postsLists = Object.keys(posts).map(function(value){
+  return posts[value];
+});
+
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/templates');
 app.use('/static', express.static(__dirname + '/public'));
@@ -17,7 +22,7 @@ app.get('/blog/:title?', function(req,res){
  
   if(title === undefined) {
     res.status(503);
-    res.send("this page is under construction");
+    res.render('blog', {posts: postsLists});
   } else {
      var post = posts[title] || {};
      res.render('post', {post: post});
